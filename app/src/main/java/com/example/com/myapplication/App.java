029fe463b5e1f7259367c87c6f3061fc344c8acf
“yangchaojianggit config --global user.email www.1007181167@qq.com <www.1007181167@qq.com>
2017-06-22 11:01:18 +0800

    增加app文件

---
+++ app/src/main/java/com/example/com/myapplication/App.java
@@ -0,0 +1,21 @@
+package com.example.com.myapplication;
+
+import android.app.Application;
+
+import com.helper.loadviewhelper.load.LoadViewHelper;
+
+/**
+ * Created by yangc on 2017/6/16.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+            LoadViewHelper.getBuilder()
+                .setLoadEmpty(R.layout.this_empty)
+                .setLoadError(R.layout.this_error)
+                .setLoadIng(R.layout.this_load_ing);
+    }
+}
